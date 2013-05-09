0840df2a9aebdd22a1bbf3ab0b40bc6d8dba94ad
James Betker <jbetker@gmail.com>
2013-05-08 23:09:35 -0700

    Implemented compass animations
    - Added in rudimentary compass readings (which don't work)
    - Added compass image that rotates around center of its access
    - Hooked things up.

--- gen/com/appliedanalog/glass/generic/R.java
+++ gen/com/appliedanalog/glass/generic/R.java
@@ -11,10 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int compass_arrow=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int label1=0x7f060000;
+        public static final int compassneedle=0x7f060000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/appliedanalog/glass/generic/MainActivity.java
+++ src/com/appliedanalog/glass/generic/MainActivity.java
@@ -1,17 +1,31 @@
 package com.appliedanalog.glass.generic;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.animation.Animator;
 import android.animation.Animator.AnimatorListener;
+import android.content.Context;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+	final String TAG = "MainActivity";
+	
+	SensorManager sensorManager;
+	Sensor magSensor;
+	Sensor accelSensor;
+	
 	TextView label1;
+	ImageView compassneedle;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +35,18 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
-		label1 = (TextView)findViewById(R.id.label1);
+		sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+		magSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		
+		//label1 = (TextView)findViewById(R.id.label1);
+		compassneedle = (ImageView)findViewById(R.id.compassneedle);
 	}
 	
-	boolean goingOut = false;
 	
 	AnimatorListener aniListener = new AnimatorListener(){
 		@Override
 		public void onAnimationEnd(Animator animation) {
-			int dest = goingOut ? 0 : 50;
-			label1.animate().x(dest).y(dest).setDuration(1000).setListener(aniListener);
-			goingOut = !goingOut;
 		}
 		
 		@Override
@@ -43,13 +58,45 @@ public class MainActivity extends Activity {
 		@Override
 		public void onAnimationStart(Animator animation) { }
 	};
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+
+	float[] latestAccelData;
+	float[] rot = new float[9];
+	float[] orient = new float[3];
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if(event.sensor == magSensor){
+			Log.v(TAG, "Mag sensor reading.");
+			if(latestAccelData == null) return;
+			if(sensorManager.getRotationMatrix(rot, null, latestAccelData, event.values)){
+				sensorManager.getOrientation(rot, orient);
+				float maginc = 180f + (float)(180f * orient[1] / Math.PI);
+				Log.v(TAG, "Inclination determined: " + maginc);
+				compassneedle.clearAnimation();
+				compassneedle.animate().rotation(maginc).setDuration(100).setListener(aniListener);
+			}
+		}else{
+			Log.v(TAG, "Accell sensor reading.");
+			latestAccelData = event.values.clone();
+		}
+		//
+	}
 	
 	@Override
 	protected void onStart(){
 		super.onStart();
 		
-		goingOut = true;
-		label1.animate().x(50).y(50).setDuration(1000).setListener(aniListener);
+		sensorManager.registerListener(this, magSensor, SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
+		//label1.animate().x(50).y(50).setDuration(1000).setListener(aniListener);
+	}
+	
+	@Override
+	protected void onStop(){
+		super.onStop();
+		sensorManager.unregisterListener(this);
 	}
 
 }
